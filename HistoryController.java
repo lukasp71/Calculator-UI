996bad8a829ebbcc6fb072cd7ca4663189de38de
lukasp71 <102604582+lukasp71@users.noreply.github.com>
2022-03-29 20:59:38 -0400

    Add files via upload

---
+++ HistoryController.java
@@ -0,0 +1,18 @@
+package calc.controllers;
+
+import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class HistoryController {
+    
+    @FXML
+    private ListView historyList;
+    
+    public void initializeCalculations(ArrayList<String> calculation_History) {
+        calculation_History.forEach((calculation) -> {
+            historyList.getItems().add(calculation);
+        });
+       
+    }
+}
